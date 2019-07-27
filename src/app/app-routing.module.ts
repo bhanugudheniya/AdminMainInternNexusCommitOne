e058b4f27102e7cd9281b115db8e86f8832e88cd
muskaanlagwal <muskaanlagwal123@gmail.com>
2019-07-27 17:47:03 +0530

    FirstCommit InternNexus Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CourseComponent } from './course/course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'course', component:CourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
